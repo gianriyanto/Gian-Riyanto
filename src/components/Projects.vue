fb553559a6caae34542cd6e4933547a4ae8eca80
gianriyanto <li.griyanto@gmail.com>
2020-11-05 21:22:43 +1100

    Added project links on Projects.vue

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -19,6 +19,15 @@
                 data-aos-duration="1000"
                 data-aos-easing="ease-in-out">
             {{ project.name }}
+            <font-awesome-icon
+                v-if="selectedProject.name === project.name"
+                class="project-link"
+                :icon="['fas', 'external-link-alt']"
+                data-aos="fade"
+                data-aos-duration="1000"
+                data-aos-easing="ease-in-out"
+                v-on:click="goToSite(project.link)">
+            </font-awesome-icon>
           </span>
           <span v-if="selectedProject" class="project-content">
             <span v-if="selectedProject.name === project.name"
@@ -57,12 +66,14 @@ export default {
         {
           id: 0,
           name: "This Personal Website",
+          link: "https://github.com/gianriyanto/Gian-Riyanto",
           description: "As you can see there isn't too much to fill in this portfolio. In all honesty, building this personal website is just another reason for me to play around with Vue.js and explore different UI/UX designs.",
           tech: ["Vue", "Vuex", "Node.js", "Netlify"]
         },
         {
           id: 1,
           name: "I OWE YOU",
+          link: "https://ioweyou.netlify.app/",
           description: "Built and deployed a favours-tracking web application with a MERN stack. " +
               "Worked primarily as a front-end developer - designed, developed and implemented the client-side application for which the product received an honourable mention for its UI/UX design.\n" +
               "Here I learnt developing clean, maintainable, and well-designed code, as well as practiced effective product management skills to ensure optimal delivery of the web app.",
@@ -71,6 +82,7 @@ export default {
         {
           id: 2,
           name: "Analytics Dashboard",
+          link: "https://github.com/davidbr4gg/analytic_dashboard",
           description: "Developed a responsive analytics dashboard for users to manage, monitor, and analyse data at a glance. Worked on the front-end development designing \n" +
               "the dashboard interface and implementing various widgets including charts, bars, cards, and tables",
           tech: ["Vue", "Vuex", "Node.js", "Chart.js"]
@@ -78,12 +90,14 @@ export default {
         {
           id: 3,
           name: "Team Chat App",
+          link: "",
           description: "Developed a team communication and project management platform with a team of 15 developers using Agile methodology and scrum framework.",
           tech: ["React", "Node.js", "Firebase"]
         },
         {
           id: 4,
           name: "Supervised Learning Project",
+          link: "https://github.com/gianriyanto/supervised-learning-project",
           description: "Implemented a machine learning classification algorithm to predict target values in a dataset. Iteratively evaluated and optimised the data pipeline to improve \n" +
               "efficiency, accuracy and performance. I employed a range of data mining, exploration, and machine learning practices to optimally solve the data analytics problem.",
           tech: ["Python", "Scikit-learn", "PyCharm", "Pandas", "NumPy", "Seaborn"]
@@ -91,8 +105,9 @@ export default {
         {
           id: 5,
           name: "Automated Data Pipeline",
-          description: "Developed a data pipeline framework to automate previously laborious multi-step tasks. Implemented Direct Acyclic Graphs to schedule and monitor workflows\n" +
-              " for various data processes. I created a comprehensive technical documentation so that it can be maintained for further business use-cases and big-data requirements.",
+          link: "https://github.com/gianriyanto/Automated-Data-Pipeline",
+          description: "Developed a data pipeline framework to automate previously laborious multi-step tasks. Programatically authored an implementation of Direct Acyclic Graphs to monitor and orchestrate workflows\n" +
+              " for various big-data processes that is currently still maintained for expanding business use-cases. ",
           tech: ["Python", "PyCharm", "Airflow", "Google Cloud Platform"]
         },
       ]
@@ -109,7 +124,10 @@ export default {
     },
     labelStyle(project) {
       return [(this.selectedProject === project) ? 'selected-project' : 'not-selected-project']
-    }
+    },
+    goToSite(url) {
+        window.open(url, '_blank');
+    },
   }
 }
 </script>
@@ -157,6 +175,16 @@ export default {
           transition: ease-out 0.4s;
         }
       }
+      .project-link{
+        margin-left: 20px;
+        position: absolute;
+        font-size: 1em;
+        transition: all .35s ease-in-out;
+
+        &:hover {
+          opacity: 0.5;
+        }
+      }
       .project-content {
         display: flex;
         flex-direction: column;
@@ -170,9 +198,10 @@ export default {
         }
 
         .tech-wrapper {
-          flex-direction: row;
           display: inline-block;
           margin-bottom: 20px;
+          flex-direction: row;
+          align-items: center;
 
           .tech {
             font-family: "Bw Modelica Regular", serif;
