2c4aa2b7167359ba4825ca0f2aa7094ead7805a7
gianriyanto <li.griyanto@gmail.com>
2020-11-06 02:10:15 +1100

    Fixed bug: Animation wasn't working

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import titleMixin from "./mixins/titleMixin";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 library.add(fab, fas);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
@@ -17,6 +19,9 @@ Vue.use(VueScrollTo);
 Vue.config.productionTip = false;
 
 new Vue({
+  created () {
+    AOS.init()
+  },
   router,
   render: h => h(App),
 }).$mount('#app')
