3fc2070da6b49467dd8c218c4c5c50f31026f80b
gianriyanto <li.griyanto@gmail.com>
2020-11-06 22:30:09 +1100

    Minor UI adjustments on About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -10,15 +10,15 @@
           data-aos="fade-left"
           data-aos-duration="1000"
           data-aos-easing="ease-in-out">
-      Hi! I'm a software engineering student (BEHons. Software) at the <span class="bold">University of Technology Sydney</span>, on track for graduation by <span class="bold">October 2021</span>. <br/> <br/>
+      I'm a software engineering student (BEHons. Software) at the <span class="bold">University of Technology Sydney</span>, on track for graduation by <span class="bold">October 2021</span>. <br/> <br/>
       Driven by my intellectual curiosity and appreciation for thoughtful design, I seek to continue my lifelong learning journey amongst a collaborative team that shares my passion for delightful products and user-experience. <br/> <br/>
       I find that nothing is more rewarding than being able to transform new learnings into something that, especially when I get to meet new people and develop relationships along the way. <br/> <br/>
       <span class="skills-headers"> Here are some technologies that I've been fiddling with recently: </span>
     </span>
     <span class="skills-wrapper"
-          data-aos="fade-up"
+          data-aos="fade-left"
           data-aos-duration="1000"
-          data-aos-easing="ease-in-sine">
+          data-aos-easing="ease-in-out">
       <span v-for="skill in skills" class="skill" :key="skill"> {{ skill }} </span>
     </span>
   </div>
@@ -40,7 +40,7 @@ export default {
 #About{
   display: flex;
   flex-direction: column;
-  padding: 100px 0 0 30px;
+  padding: 80px 0 0 30px;
   margin: auto;
   color: white;
   width: 630px;
