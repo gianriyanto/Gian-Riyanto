da698f9ef2c61592656091a061cb0ab930370f52
gianriyanto <li.griyanto@gmail.com>
2020-11-07 11:06:25 +1100

    Fixed bug

--- src/components/About.vue
+++ src/components/About.vue
@@ -11,8 +11,8 @@
           data-aos-duration="1000"
           data-aos-easing="ease-in-out">
       I'm a software engineering student (BEHons. Software) at the <span class="bold">University of Technology Sydney</span>, on track for graduation by <span class="bold">October 2021</span>. <br/> <br/>
-      Driven by my intellectual curiosity and appreciation for thoughtful design, I seek to continue my lifelong learning journey amongst a collaborative team that shares my passion for delightful products and user-experience. <br/> <br/>
-      I find that nothing is more rewarding than being able to transform new learnings into something that, especially when I get to meet new people and develop relationships along the way. <br/> <br/>
+      Driven by my intellectual curiosity and appreciation for thoughtful design, I seek to continue my lifelong learning journey amongst a collaborative team that shares my passion for products and user-experience. <br/> <br/>
+      I find that nothing is more rewarding than being able to transform new learnings into something users find delightful, especially when I get to meet new people and develop relationships along the way. <br/> <br/>
       <span class="skills-headers"> Here are some technologies that I've been fiddling with recently: </span>
     </span>
     <span class="skills-wrapper"
@@ -40,7 +40,7 @@ export default {
 #About{
   display: flex;
   flex-direction: column;
-  padding: 80px 0 0 30px;
+  padding: 60px 0 0 30px;
   margin: auto;
   color: white;
   width: 630px;
@@ -60,7 +60,7 @@ export default {
 
 .skills-wrapper{
   flex-direction: row;
-  display: inline-block;
+  display: flex;
 
   .skill {
     font-family: "Bw Modelica Bold", serif;
