3f5c0c02545c34ede49a5ae23cfd0f5b608468a0
gianriyanto <li.griyanto@gmail.com>
2020-11-10 12:56:15 +1100

    Minor UI changes on About.vue and edited content on About me Message

--- src/components/About.vue
+++ src/components/About.vue
@@ -36,8 +36,8 @@ export default {
     return {
       skills: ["Javascript", "React", "Vue", "HTML", "(S)CSS", "Node.js", "Python"],
       aboutMe: `Hey there, I am a software engineer with an obsession for detail and a passion for value creation. I am currently studying (BEHons. Software) at the University of Technology Sydney, on track for graduation by October 2021.
-My greatest pleasure comes from being able to engage with challenging problems, uncovering customer pain points, and seek out new solutions for the next software iteration.
-From decomposing high-level problems down to single lines of code to learning new technologies to better approach a solution, I love to solve problems.
+My greatest pleasure comes from being able to dwell on creating thoughtful user experience, uncovering customer pain points, and seek out new solutions for the next software iteration.
+From decomposing high-level problems down to single lines of code, to tinkering with new technologies and frameworks, I enjoy the collaborative process of solving interesting problems.
 I believe nothing is more rewarding than an opportunity to transform new things I learn into something users find delightful, especially when I get to meet new people and develop relationships along the way.`
     }
   }
@@ -86,7 +86,7 @@ I believe nothing is more rewarding than an opportunity to transform new things 
       font-size: 12px;
       width: fit-content;
       padding: 8px 1vw;
-      margin-right: 1vw;
+      margin-right: 0.7vw;
     }
   }
 }
